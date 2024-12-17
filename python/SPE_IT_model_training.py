91894385480fb716cc7c93879b9bb14372df6eca
fchabrun <floris.chabrun@chu-angers.fr>
2024-12-17 09:59:34 +0100

    jz8

--- python/SPE_IT_model_training.py
+++ python/SPE_IT_model_training.py
@@ -21,6 +21,44 @@ import lightning as pl
 from lightning.pytorch.callbacks import ModelCheckpoint, EarlyStopping
 from lightning.pytorch.loggers import CSVLogger
 
+
+# a function for plotting
+def plot_IT_predictions(sample_x, sample_y, sample_pred):
+    plt.figure(figsize=(14, 10))
+    plt.subplot(3, 1, 1)
+    # on récupère la class map (binarisée)
+    # class_map = y[ix].max(axis=1)
+    curve_values = sample_x
+    for num, col, lab in zip(range(6), ['black', 'purple', 'pink', 'green', 'red', 'blue'],
+                             ['Ref', 'G', 'A', 'M', 'k', 'l']):
+        plt.plot(np.arange(0, 304), curve_values[:, num], '-', color=col, label=lab)
+    plt.title('Valid set curve index {}'.format(ix))
+
+    plt.legend()
+    # for peak_start, peak_end in zip(np.where(np.diff(class_map)==1)[0]+1, np.where(np.diff(class_map)==-1)[0]+1):
+    #     plt.plot(np.arange(peak_start,peak_end), curve_values[peak_start:peak_end], '-', color = 'red')
+
+    # on plot aussi les autres courbes
+    plt.subplot(3, 1, 2)
+    for num, col, lab in zip(range(5), ['purple', 'pink', 'green', 'red', 'blue'], ['G', 'A', 'M', 'k', 'l']):
+        plt.plot(np.arange(0, 304) + 1, sample_y[:, num] / 5 + (4 - num) / 5, '-', color=col, label=lab)
+    plt.ylim(-.05, 1.05)
+    plt.legend()
+    plt.title('Ground truth maps')
+
+    plt.subplot(3, 1, 3)
+    for num, col, lab in zip(range(5), ['purple', 'pink', 'green', 'red', 'blue'], ['G', 'A', 'M', 'k', 'l']):
+        plt.plot(np.arange(0, 304) + 1, sample_pred[num, :] / 5 + (4 - num) / 5, '-', color=col, label=lab)
+    plt.ylim(-.05, 1.05)
+    plt.legend()
+    plt.title('Predicted maps')
+    if args.debug != "inline":
+        plt.savefig(os.path.join(args.debug, f"pred_plot_{ix=}.png"))
+        plt.close()
+    else:
+        plt.show()
+
+
 # %%
 
 if __name__ == "__main__":
@@ -290,185 +328,150 @@ if __name__ == "__main__":
     Validation metrics
     ============================================================================="""
 
-    # reload last = best (hopefully?) checkpoint
-    if config.architecture == "segformer":
-        model = pl_IS_model.load_from_checkpoint(os.path.join(args.output_root_path, "last.ckpt"))
-    else:
-        assert False, f"Unhandled {config.architecture=}"
+    if args.run_mode in ("valid", "full"):
 
-    trainer = pl.Trainer(
-        default_root_dir=args.output_root_path,
-        **trainer_args,
-        enable_progress_bar=console_verbose,
-    )
-
-    # predict on validation data
-    validation_outputs = trainer.predict(model, dataloaders=validation_loader)
-    # note: in pytorch, the output is a list of N elements, N being the number of batches => so we have to convert that to a np array
-    validation_preds = torch.cat(validation_outputs).detach().cpu().numpy()
-
-    export_metrics = dict()
-
-    # some general metrics : point precision and IOU
-    threshold = .5
-    points = np.arange(1, 304 + 1, 1)
-    pr = np.zeros((validation_preds.shape[0], 5))
-    iou = np.zeros((validation_preds.shape[0], 5))
-    for ix in range(validation_preds.shape[0]):
-        for dim in range(5):
-            gt = if_y_test[ix, :, dim]
-            pd_ = (validation_preds[ix, dim, :] > threshold) * 1
-            u = np.sum(gt + pd_ > 0)
-            i = np.sum(gt + pd_ == 2)
-            if np.isfinite(u):
-                iou[ix, dim] = i / u
-            else:
-                iou[ix, dim] = np.nan
-            pr[ix, dim] = np.sum(gt == pd_) / 304
-
-    for k in range(iou.shape[1]):
-        print("Mean IoU for fraction '{}': {:.2f} +- {:.2f}".format(['G', 'A', 'M', 'k', 'l'][k], np.nanmean(iou[:, k]),
-                                                                    np.nanstd(iou[:, k])))
-        export_metrics['IoU-{}'.format(['G', 'A', 'M', 'k', 'l'][k])] = np.nanmean(iou[:, k])
-    export_metrics['IoU-global'] = np.nanmean(iou)
-
-    for k in range(pr.shape[1]):
-        print("Mean accuracy for fraction '{}': {:.2f} +- {:.2f}".format(['G', 'A', 'M', 'k', 'l'][k], np.nanmean(pr[:, k]),
-                                                                         np.nanstd(pr[:, k])))
-
-
-    # a function for plotting
-    def plotITPredictions(ix):
-        plt.figure(figsize=(14, 10))
-        plt.subplot(3, 1, 1)
-        # on récupère la class map (binarisée)
-        # class_map = y[ix].max(axis=1)
-        curve_values = if_x_test[ix, :, :]
-        for num, col, lab in zip(range(6), ['black', 'purple', 'pink', 'green', 'red', 'blue'],
-                                 ['Ref', 'G', 'A', 'M', 'k', 'l']):
-            plt.plot(np.arange(0, 304), curve_values[:, num], '-', color=col, label=lab)
-        plt.title('Valid set curve index {}'.format(ix))
-
-        plt.legend()
-        # for peak_start, peak_end in zip(np.where(np.diff(class_map)==1)[0]+1, np.where(np.diff(class_map)==-1)[0]+1):
-        #     plt.plot(np.arange(peak_start,peak_end), curve_values[peak_start:peak_end], '-', color = 'red')
-
-        # on plot aussi les autres courbes
-        plt.subplot(3, 1, 2)
-        for num, col, lab in zip(range(5), ['purple', 'pink', 'green', 'red', 'blue'], ['G', 'A', 'M', 'k', 'l']):
-            plt.plot(np.arange(0, 304) + 1, if_y_test[ix, :, num] / 5 + (4 - num) / 5, '-', color=col, label=lab)
-        plt.ylim(-.05, 1.05)
-        plt.legend()
-        plt.title('Ground truth maps')
-
-        plt.subplot(3, 1, 3)
-        for num, col, lab in zip(range(5), ['purple', 'pink', 'green', 'red', 'blue'], ['G', 'A', 'M', 'k', 'l']):
-            plt.plot(np.arange(0, 304) + 1, validation_preds[ix, num, :] / 5 + (4 - num) / 5, '-', color=col, label=lab)
-        plt.ylim(-.05, 1.05)
-        plt.legend()
-        plt.title('Predicted maps')
-        if args.debug != "inline":
-            plt.savefig(os.path.join(args.debug, f"pred_plot_{ix=}.png"))
-            plt.close()
+        # reload last = best (hopefully?) checkpoint
+        if config.architecture == "segformer":
+            model = pl_IS_model.load_from_checkpoint(os.path.join(args.output_root_path, "last.ckpt"))
         else:
-            plt.show()
+            assert False, f"Unhandled {config.architecture=}"
 
+        trainer = pl.Trainer(
+            default_root_dir=args.output_root_path,
+            **trainer_args,
+            enable_progress_bar=console_verbose,
+        )
 
-    if args.debug is not None:
-        for idx in [0, 1, 2]:
-            try:
-                plotITPredictions(idx)
-            except:
-                print(f"Unable to plot validation preds for {idx=}")
+        # predict on validation data
+        validation_outputs = trainer.predict(model, dataloaders=validation_loader)
+        # note: in pytorch, the output is a list of N elements, N being the number of batches => so we have to convert that to a np array
+        validation_preds = torch.cat(validation_outputs).detach().cpu().numpy()
 
-    # Calculons pour chaque pic réel/prédit la concordance
-    threshold = 0.5  # ou 0.5
-    curve_ids = []
-    groundtruth_spikes = []
-    predicted_spikes = []
-    for ix in range(if_x_test.shape[0]):
-        flat_gt = np.zeros_like(if_y_test[ix, :, 0])
-        for i in range(if_y_test.shape[-1]):
-            flat_gt += if_y_test[ix, :, i] * (1 + np.power(2, i))
-        gt_starts = []
-        gt_ends = []
-        prev_v = 0
-        for i in range(304):
-            if flat_gt[i] != prev_v:  # changed
-                # multiple cases:
-                # 0 -> non-zero = enter peak
-                if prev_v == 0:
-                    gt_starts.append(i)
-                # non-zero -> 0 = out of peak
-                elif flat_gt[i] == 0:
-                    gt_ends.append(i)
-                # non-zero -> different non-zero = enter other peak
+        export_metrics = dict()
+
+        # some general metrics : point precision and IOU
+        threshold = .5
+        points = np.arange(1, 304 + 1, 1)
+        pr = np.zeros((validation_preds.shape[0], 5))
+        iou = np.zeros((validation_preds.shape[0], 5))
+        for ix in range(validation_preds.shape[0]):
+            for dim in range(5):
+                gt = if_y_test[ix, :, dim]
+                pd_ = (validation_preds[ix, dim, :] > threshold) * 1
+                u = np.sum(gt + pd_ > 0)
+                i = np.sum(gt + pd_ == 2)
+                if np.isfinite(u):
+                    iou[ix, dim] = i / u
                 else:
-                    gt_ends.append(i)
-                    gt_starts.append(i)
-                prev_v = flat_gt[i]
+                    iou[ix, dim] = np.nan
+                pr[ix, dim] = np.sum(gt == pd_) / 304
 
-        if len(gt_starts) != len(gt_ends):
-            raise Exception('Inconsistent start/end points')
+        for k in range(iou.shape[1]):
+            print("Mean IoU for fraction '{}': {:.2f} +- {:.2f}".format(['G', 'A', 'M', 'k', 'l'][k], np.nanmean(iou[:, k]),
+                                                                        np.nanstd(iou[:, k])))
+            export_metrics['IoU-{}'.format(['G', 'A', 'M', 'k', 'l'][k])] = np.nanmean(iou[:, k])
+        export_metrics['IoU-global'] = np.nanmean(iou)
 
-        if len(gt_starts) > 0:
-            # TODO => ce type de validation ne prend pas en compte les faux positifs!
-            # pour chaque pic, on détecte ce que le modèle a rendu a cet endroit comme type d'Ig
-            for pstart, pend in zip(gt_starts, gt_ends):
-                gt_ig_denom = ''
-                if np.sum(if_y_test[ix, pstart:pend, :3]) > 0:
-                    HC_gt = int(np.median(np.argmax(if_y_test[ix, pstart:pend, :3], axis=1)))
-                    gt_ig_denom = ['G', 'A', 'M'][HC_gt]
-                lC_gt = int(np.median(np.argmax(if_y_test[ix, pstart:pend, 3:], axis=1)))
-                gt_ig_denom += ['k', 'l'][lC_gt]
+        for k in range(pr.shape[1]):
+            print("Mean accuracy for fraction '{}': {:.2f} +- {:.2f}".format(['G', 'A', 'M', 'k', 'l'][k], np.nanmean(pr[:, k]),
+                                                                             np.nanstd(pr[:, k])))
 
+
+        if args.debug is not None:
+            for idx in [0, 1, 2]:
+                try:
+                    plot_IT_predictions(if_x_test[idx, ...], if_y_test[idx, ...], validation_preds[idx, ...])
+                except:
+                    print(f"Unable to plot validation preds for {idx=}")
+
+        # Calculons pour chaque pic réel/prédit la concordance
+        threshold = 0.5  # ou 0.5
+        curve_ids = []
+        groundtruth_spikes = []
+        predicted_spikes = []
+        for ix in range(if_x_test.shape[0]):
+            flat_gt = np.zeros_like(if_y_test[ix, :, 0])
+            for i in range(if_y_test.shape[-1]):
+                flat_gt += if_y_test[ix, :, i] * (1 + np.power(2, i))
+            gt_starts = []
+            gt_ends = []
+            prev_v = 0
+            for i in range(304):
+                if flat_gt[i] != prev_v:  # changed
+                    # multiple cases:
+                    # 0 -> non-zero = enter peak
+                    if prev_v == 0:
+                        gt_starts.append(i)
+                    # non-zero -> 0 = out of peak
+                    elif flat_gt[i] == 0:
+                        gt_ends.append(i)
+                    # non-zero -> different non-zero = enter other peak
+                    else:
+                        gt_ends.append(i)
+                        gt_starts.append(i)
+                    prev_v = flat_gt[i]
+
+            if len(gt_starts) != len(gt_ends):
+                raise Exception('Inconsistent start/end points')
+
+            if len(gt_starts) > 0:
+                # TODO => ce type de validation ne prend pas en compte les faux positifs!
+                # pour chaque pic, on détecte ce que le modèle a rendu a cet endroit comme type d'Ig
+                for pstart, pend in zip(gt_starts, gt_ends):
+                    gt_ig_denom = ''
+                    if np.sum(if_y_test[ix, pstart:pend, :3]) > 0:
+                        HC_gt = int(np.median(np.argmax(if_y_test[ix, pstart:pend, :3], axis=1)))
+                        gt_ig_denom = ['G', 'A', 'M'][HC_gt]
+                    lC_gt = int(np.median(np.argmax(if_y_test[ix, pstart:pend, 3:], axis=1)))
+                    gt_ig_denom += ['k', 'l'][lC_gt]
+
+                    pred_ig_denom = ''
+                    if np.sum(validation_preds[ix, :, pstart:pend] > threshold) > 0:  # un pic a été détecté
+                        if np.sum(validation_preds[ix, :3, pstart:pend] > threshold) > 0:
+                            HC_pred = int(np.median(np.argmax(validation_preds[ix, :3, pstart:pend], axis=0)))
+                            pred_ig_denom = ['G', 'A', 'M'][HC_pred]
+                        lC_pred = int(np.median(np.argmax(validation_preds[ix, 3:, pstart:pend], axis=0)))
+                        pred_ig_denom += ['k', 'l'][lC_pred]
+                    else:
+                        pred_ig_denom = 'none'
+
+                    groundtruth_spikes.append(gt_ig_denom)
+                    predicted_spikes.append(pred_ig_denom)
+                    curve_ids.append(ix)
+            else:
+                gt_ig_denom = 'none'
                 pred_ig_denom = ''
-                if np.sum(validation_preds[ix, :, pstart:pend] > threshold) > 0:  # un pic a été détecté
-                    if np.sum(validation_preds[ix, :3, pstart:pend] > threshold) > 0:
-                        HC_pred = int(np.median(np.argmax(validation_preds[ix, :3, pstart:pend], axis=0)))
-                        pred_ig_denom = ['G', 'A', 'M'][HC_pred]
-                    lC_pred = int(np.median(np.argmax(validation_preds[ix, 3:, pstart:pend], axis=0)))
-                    pred_ig_denom += ['k', 'l'][lC_pred]
-                else:
-                    pred_ig_denom = 'none'
+                if np.sum(validation_preds[ix, :3, :] > threshold) > 0:
+                    HC_pred = int(np.median(np.argmax(validation_preds[ix, :3, :], axis=0)))
+                    pred_ig_denom = ['G', 'A', 'M'][HC_pred]
+                lC_pred = int(np.median(np.argmax(validation_preds[ix, 3:, :], axis=0)))
+                pred_ig_denom += ['k', 'l'][lC_pred]
 
                 groundtruth_spikes.append(gt_ig_denom)
                 predicted_spikes.append(pred_ig_denom)
                 curve_ids.append(ix)
-        else:
-            gt_ig_denom = 'none'
-            pred_ig_denom = ''
-            if np.sum(validation_preds[ix, :3, :] > threshold) > 0:
-                HC_pred = int(np.median(np.argmax(validation_preds[ix, :3, :], axis=0)))
-                pred_ig_denom = ['G', 'A', 'M'][HC_pred]
-            lC_pred = int(np.median(np.argmax(validation_preds[ix, 3:, :], axis=0)))
-            pred_ig_denom += ['k', 'l'][lC_pred]
 
-            groundtruth_spikes.append(gt_ig_denom)
-            predicted_spikes.append(pred_ig_denom)
-            curve_ids.append(ix)
+        conc_df = pd.DataFrame(dict(ix=curve_ids,
+                                    true=groundtruth_spikes,
+                                    pred=predicted_spikes))
 
-    conc_df = pd.DataFrame(dict(ix=curve_ids,
-                                true=groundtruth_spikes,
-                                pred=predicted_spikes))
+        print(pd.crosstab(conc_df.true, conc_df.pred))
 
-    print(pd.crosstab(conc_df.true, conc_df.pred))
+        print('Global precision: ' + str(round(100 * np.sum(conc_df.true == conc_df.pred) / conc_df.shape[0], 1)))
+        for typ in np.unique(conc_df.true):
+            subset = conc_df.true == typ
+            print('  Precision for type ' + typ + ': ' + str(
+                round(100 * np.sum(conc_df.true.loc[subset] == conc_df.pred.loc[subset]) / np.sum(subset), 1)))
+            export_metrics['Acc-{}'.format(typ)] = 100 * np.sum(conc_df.true.loc[subset] == conc_df.pred.loc[subset]) / np.sum(
+                subset)
+        export_metrics['Acc-global'] = 100 * np.sum(conc_df.true == conc_df.pred) / conc_df.shape[0]
 
-    print('Global precision: ' + str(round(100 * np.sum(conc_df.true == conc_df.pred) / conc_df.shape[0], 1)))
-    for typ in np.unique(conc_df.true):
-        subset = conc_df.true == typ
-        print('  Precision for type ' + typ + ': ' + str(
-            round(100 * np.sum(conc_df.true.loc[subset] == conc_df.pred.loc[subset]) / np.sum(subset), 1)))
-        export_metrics['Acc-{}'.format(typ)] = 100 * np.sum(conc_df.true.loc[subset] == conc_df.pred.loc[subset]) / np.sum(
-            subset)
-    export_metrics['Acc-global'] = 100 * np.sum(conc_df.true == conc_df.pred) / conc_df.shape[0]
+        export_metrics['Mistakes-total'] = conc_df.loc[conc_df.true != conc_df.pred, :].shape[0]
+        mistakes = conc_df.loc[conc_df.true != conc_df.pred, 'ix'].unique().tolist()
+        export_metrics['Mistakes-curves'] = len(mistakes)
 
-    export_metrics['Mistakes-total'] = conc_df.loc[conc_df.true != conc_df.pred, :].shape[0]
-    mistakes = conc_df.loc[conc_df.true != conc_df.pred, 'ix'].unique().tolist()
-    export_metrics['Mistakes-curves'] = len(mistakes)
-
-    # export predictions
-    np.save(os.path.join(args.output_root_path, "validation_preds.npy"), validation_preds)
-    # export metrics
-    with open(os.path.join(args.output_root_path, "export_metrics.json"), "w") as jsf:
-        json.dump(export_metrics, jsf, indent=4)
+        # export predictions
+        np.save(os.path.join(args.output_root_path, "validation_preds.npy"), validation_preds)
+        # export metrics
+        with open(os.path.join(args.output_root_path, "export_metrics.json"), "w") as jsf:
+            json.dump(export_metrics, jsf, indent=4)
